64aad47f3958d7ab13539a303dfb87eaf1adbb05
devy52 <108571763+devy52@users.noreply.github.com>
2025-04-02 10:09:23 +0530

    Delete realbasicvsr/models/registry.py

--- realbasicvsr/models/registry.py
+++
@@ -1,7 +0,0 @@
-# from mmcv.utils import Registry
-from mmengine.registry import Registry
-
-MODELS = Registry('model')
-BACKBONES = Registry('backbone')
-COMPONENTS = Registry('component')
-LOSSES = Registry('loss')
